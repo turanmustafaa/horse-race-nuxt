418c0b35e83fc28398bc5ce4d604eb2eaa63e872
turanmustafaa <57253120+turanmustafaa@users.noreply.github.com>
2024-09-16 13:16:16 +0300

    Update HorseSound.vue

--- components/HorseSound.vue
+++ components/HorseSound.vue
@@ -1,34 +1,33 @@
 <template>
   <div>
     <audio ref="backgroundMusic" src="/gifs/horse-sound.wav" loop></audio>
-
-    <audio ref="specialMusic" src="/gifs/special.mp3" loop></audio>
   </div>
 </template>
 
 <script setup lang="ts">
+  //<audio ref="specialMusic" src="/gifs/special.mp3" loop></audio>
 import { ref, watch } from 'vue'
 import { useHorseStore } from '@/stores/useHorseStore'
 
 const horseStore = useHorseStore()
 const backgroundMusic = ref<HTMLAudioElement | null>(null)
-const specialMusic = ref<HTMLAudioElement | null>(null)
+//const specialMusic = ref<HTMLAudioElement | null>(null)
 
 watch(() => horseStore.playState, (newVal) => {
   if (newVal) {
     backgroundMusic.value?.play() 
   } else {
     backgroundMusic.value?.pause()
-    specialMusic.value?.pause()
+    //specialMusic.value?.pause()
   }
 })
 
-watch(() => horseStore.runHorses, (newVal) => {
-  const hasSpecialHorse = newVal.some(horse => horse.name === 'Sahbatur' || horse.name === 'Gulbatur')
-  if (hasSpecialHorse && horseStore.playState) {
-    specialMusic.value?.play()
-  } else {
-    specialMusic.value?.pause()
-  }
-}, { deep: true })
+//watch(() => horseStore.runHorses, (newVal) => {
+  //const hasSpecialHorse = newVal.some(horse => horse.name === 'Sahbatur' || horse.name === 'Gulbatur')
+  //if (hasSpecialHorse && horseStore.playState) {
+    //specialMusic.value?.play()
+  //} else {
+    //specialMusic.value?.pause()
+  //}
+//}, { deep: true })
 </script>
